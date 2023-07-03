054ff18ab9719ef97a47415c517ac9fc60ec2ec7
Kaya Sertel <50380412+KayaSrtl@users.noreply.github.com>
2023-07-03 21:43:51 +0300

    Create script.js

---
+++ files/script.js
@@ -0,0 +1,50 @@
+var window_height, window_width;
+//var ismenuopen = false;
+//var is_mobile_phone = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
+
+
+$( document ).ready(function() {
+	beReadyPage();
+	setTimeout(function() { beReadyPage();}, 100);
+	setTimeout(function() { beReadyPage();}, 1000);
+});
+
+
+$(document).keydown(function(e){
+	//e.preventDefault();
+	
+	//alert(e.keyCode);
+	
+	if(e.keyCode == 13)
+		changeJsonCurrentTime();
+});
+
+
+
+
+$(window).scroll(function(event){
+	
+	
+	
+	
+});
+
+
+function beReadyPage () {
+	window_height = parseInt($( window ).height());
+	window_width = parseInt($( window ).width());
+}
+
+
+
+
+$( window ).resize(function() {
+	beReadyPage();
+	setTimeout(function() { beReadyPage();}, 100);
+	return;
+});
+
+
+function changeJsonCurrentTime() {
+  
+}
